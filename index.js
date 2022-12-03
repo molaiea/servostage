3f5d93d52c13cc2f535ef4cfe2a72472e044a5ac
molaiea <atlaghnafissa@iav.ac.ma>
2022-12-03 22:47:30 +0100

    addelements

--- index.js
+++ index.js
@@ -1,21 +1,34 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const path = require('path');
-const client = require('./connection');
-const cors = require('cors');
-
+import express, { query } from 'express'
+import pg from "pg";
+import cors from 'cors';
+import bodyParser from 'body-parser';
 const app = express();
+import mydata from './Bureau_TOPO.json' assert { type: "json" };
+console.log(mydata)
 app.use(cors())
 
 app.use(express.static("public"));
 app.use(express.json());
 app.use(bodyParser.json());
-
-let data;
-
+const DATABASE_URL = "postgresql://postgres:qljEtxAsWJCXwG8ix30d@containers-us-west-143.railway.app:8064/railway";
+const pool = new pg.Pool({
+    DATABASE_URL,
+});
+app.post('/add_all', (req, res)=>{
+    mydata.features.forEach((feature)=>{
+        var geom = `POINT(${feature.geometry.coordinates[0]} ${feature.geometry.coordinates[1]})`
+        var offre_ing = feature.properties.Offre_ing;
+        var offre_tech = feature.properties.Offre_tech;
+        var nom = feature.properties.Nom;
+        var query_string = `INSERT INTO bureau_topo(nom, offre_ing, offre_tech, geom) VALUES('${nom}', '${offre_ing}', '${offre_tech}', ST_GeomFromText('${geom}', 4326));`;
+        pool.query(query_string);
+    })
+    res.send('done')
+})
 app.post("/api", (request, response) => {
   console.log("I got a request !");
   console.log(request.body);
+
   data = request.body;
   response.json({
     status: "success",
