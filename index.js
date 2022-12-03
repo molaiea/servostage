878622cbbefaf166f66161fce2b290f666de50dc
molaiea <atlaghnafissa@iav.ac.ma>
2022-12-03 22:55:27 +0100

    ldhk

--- index.js
+++ index.js
@@ -5,10 +5,8 @@ import bodyParser from 'body-parser';
 const app = express();
 import mydata from './Bureau_TOPO.json' assert { type: "json" };
 app.use(cors())
-
-app.use(express.static("public"));
-app.use(express.json());
 app.use(bodyParser.json());
+
 const DATABASE_URL = "postgresql://postgres:qljEtxAsWJCXwG8ix30d@containers-us-west-143.railway.app:8064/railway";
 const pool = new pg.Pool({
     DATABASE_URL,
